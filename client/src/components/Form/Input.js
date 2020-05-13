3bddb59254610ae34f3d14028e7184f40982b61a
Wayne Wu <mrwu42@gmail.com>
2020-05-12 20:05:03 -0400

    google book search

---
+++ client/src/components/Form/Input.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Label } from "./Label";
+
+export const Input = props => (
+  <div className="form-group">
+    <Label htmlFor={props.name}>{props.label}</Label>
+    <input className="form-control" {...props}/>
+  </div>
+);
+
